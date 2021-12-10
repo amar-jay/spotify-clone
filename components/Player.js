fef232eeae3112549fcadd420e4420baf35a1ce1
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2021-12-10 19:00:42 +0300

    End of Day 1

--- components/Player.js
+++ components/Player.js
@@ -5,8 +5,8 @@ import styles from '../styles/Player.module.css'
 function Player() {
     return (
         <div className={styles.player}>
+           <p>PLAYING... ♫ <span>Break the Bones</span></p>
            <Image objectFit='center' src="/favicon.ico" alt="Player Image" width={20} height={20} />
-           Currently Playing...{' '}
         </div>
     )
 }
